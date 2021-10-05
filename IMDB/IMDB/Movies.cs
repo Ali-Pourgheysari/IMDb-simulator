1494e922972720cd2142a0a4db7642a3b2ea077c
Gheysar007 <a.geysari@gmail.com>
2021-10-05 13:55:25 +0330

    this is my seccond commit

---
+++ IMDB/IMDB/Movies.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMDB
+{
+    public partial class Movies : Form
+    {
+        public Movies()
+        {
+            InitializeComponent();
+        }
+
+        private void Movies_Load(object sender, EventArgs e)
+        {
+            ReadData data = new ReadData();
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.DataSource = data.gridviewcall;
+        }
+    }
+}
--- IMDB/IMDB/ReadData.cs
+++ IMDB/IMDB/ReadData.cs
@@ -11,7 +11,62 @@ namespace IMDB
 {
     class ReadData
     {
-        
+        public object gridviewcall;
+
+        public ReadData()
+        {
+            string DocumentAddress = Application.StartupPath;
+            string NewAddress = DocumentAddress.Remove(DocumentAddress.Length - 19);
+            string[] akas = File.ReadAllLines(NewAddress + "title.akas.txt");
+            string[] ratings = File.ReadAllLines(NewAddress + "title.ratings.txt");
+
+            List<ImdbAkas> AkasDataList = new List<ImdbAkas>();
+            List<ImdbRatings> RatingsDataList = new List<ImdbRatings>();
+
+            string[] TempForAddingAkasToList;
+            string[] TempForAddingRatingsToList;
+
+
+            for (int i = 1; i < akas.Length; i++)
+            {
+                TempForAddingAkasToList = akas[i].Split('\t');
+                TempForAddingRatingsToList = ratings[i].Split('\t');
+                AkasDataList.Add(new ImdbAkas()
+                {
+                    TitleId = TempForAddingAkasToList[0],
+                    Ordering = int.Parse(TempForAddingAkasToList[1]),
+                    Title = TempForAddingAkasToList[2],
+                    Region = TempForAddingAkasToList[3],
+                    Language = TempForAddingAkasToList[4],
+                    Types = TempForAddingAkasToList[5],
+                    Attributes = TempForAddingAkasToList[6],
+                    IsOriginalTitle = (int.Parse(TempForAddingAkasToList[7]) == 1) ? 1 : 0,
+                });
+                RatingsDataList.Add(new ImdbRatings()
+                {
+                    Tconst = TempForAddingRatingsToList[0],
+                    AverageRating = float.Parse(TempForAddingRatingsToList[1], CultureInfo.InvariantCulture.NumberFormat),
+                    NumVotes = int.Parse(TempForAddingRatingsToList[2])
+                });
+            }
+
+            var innerjoin = (from a in AkasDataList
+                             join r in RatingsDataList on a.TitleId equals r.Tconst
+                             select new
+                             {
+                                 a.TitleId,
+                                 a.Title,
+                                 a.Ordering,
+                                 a.Region,
+                                 a.Language,
+                                 a.Types,
+                                 a.Attributes,
+                                 a.IsOriginalTitle,
+                                 r.AverageRating,
+                                 r.NumVotes
+                             });
+            gridviewcall = innerjoin.ToList();
+        }
 
 
 
